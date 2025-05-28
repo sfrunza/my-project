2c30cdb8e4cb3f99f2b715c69ca98f8194079463
sfrunza <frunza.sergiu3@gmail.com>
2025-05-27 21:38:19 -0400

    auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,36 @@
-import { Button } from '@/components/ui/button';
-import { CardWithForm } from './card-with-form';
+import { ThemeProvider } from '@/components/theme-provider';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router';
+import { Toaster } from 'sonner';
+import ErrorPage from '@/pages/error/page';
+import { authRoutes } from '@/routes/auth.routes';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div className="flex flex-col gap-10">
+        Hello world!
+        <Link to="/auth/login">Login</Link>
+      </div>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  ...authRoutes,
+  // crmRoutes,
+  // accountRoutes,
+  // dashboardRoutes
+]);
 
 function App() {
   return (
-    <div className="bg-red-500 min-h-screen">
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <Button>Click me</Button>
-      <CardWithForm />
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+      <Toaster
+        position="top-center"
+        richColors
+        // closeButton
+      />
+    </ThemeProvider>
   );
 }
 
